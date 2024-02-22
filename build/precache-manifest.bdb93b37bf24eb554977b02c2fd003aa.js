9761ec11961c65a2b7521b7781f672a9ce30ca23
Harry <harrymian@scoutsecure.com>
2024-02-21 19:47:40 -0500

    build foler

---
+++ build/precache-manifest.bdb93b37bf24eb554977b02c2fd003aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b90eae19725f3c0d6f6d380fb4ef189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39351ba44824101de34e",
+    "url": "/static/css/main.48e59daa.chunk.css"
+  },
+  {
+    "revision": "65b610d8443a40878fe4",
+    "url": "/static/js/2.21e000c6.chunk.js"
+  },
+  {
+    "revision": "54c8a9647af8e8efb508125128896136",
+    "url": "/static/js/2.21e000c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39351ba44824101de34e",
+    "url": "/static/js/main.6a48d99f.chunk.js"
+  },
+  {
+    "revision": "73e3703058bdea6c1b6d",
+    "url": "/static/js/runtime-main.f59cf6c7.js"
+  }
+]);
